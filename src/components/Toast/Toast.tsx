ebebfb18cd9bec99885ba2d389c49efc22ea5f7e
PrincePRS <princeprs@163.com>
2023-06-01 16:18:45 +0800

    update 2023-06-01:4PM

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Toast as ToastPrimitive } from "@100mslive/react-ui";
+
+interface ToastDataProps {
+  title: string;
+  description?: string;
+  close: boolean;
+  open: boolean;
+  duration: number;
+  isClosable?: boolean;
+  onOpenChange: (open: boolean) => void;
+  icon?: React.ReactNode;
+}
+
+export const Toast: React.FC<ToastDataProps> = ({
+  title,
+  description,
+  close = true,
+  open,
+  duration,
+  onOpenChange,
+  icon,
+  ...props
+}) => {
+  return (
+    <ToastPrimitive.HMSToast
+      title={title}
+      description={description}
+      open={open}
+      isClosable={close}
+      onOpenChange={onOpenChange}
+      duration={!close ? 600000 : duration}
+      icon={icon}
+      {...props}
+    />
+  );
+};
---
+++ src/components/hooks/useUserPreferences.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useLocalStorage } from "react-use";
+
+export const UserPreferencesKeys = {
+  PREVIEW: "preview",
+  NOTIFICATIONS: "notifications",
+  UI_SETTINGS: "uiSettings",
+  RTMP_URLS: "rtmpUrls",
+};
+
+export const defaultPreviewPreference = {
+  name: "",
+  isAudioMuted: false,
+  isVideoMuted: false,
+};
+
+export const useUserPreferences = (
+  key: string,
+  defaultPreference: Record<string, any>
+) => {
+  const [localStorageValue, setStorageValue] = useLocalStorage(
+    key,
+    defaultPreference
+  );
+  const [preference, setPreference] = useState(
+    localStorageValue || defaultPreference
+  );
+  const changePreference = (value: Record<string, any>) => {
+    setPreference(value);
+    setStorageValue(value);
+  };
+  return [preference, changePreference];
+};
